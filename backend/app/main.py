ba7f45be2e6703041ac2e5a3be8fb7d3bb50e00f
Octogonn <oc866988@ucf.edu>
2026-02-13 19:05:06 -0500

    added a test dict for where we keep frame Ids also tested them out they worked

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,9 +3,8 @@ from fastapi import FastAPI
 from app.routes import health, camera, incidents, stream
 
 app = FastAPI()
+api_prefix = "/api"
 # giving all the routers direct acess to the main APP
 app.include_router(health.router)
-app.include_router(camera.router)
-app.include_router(incidents.router)
-app.include_router(stream.router)
+app.include_router(camera.router, prefix = api_prefix)
 
--- backend/app/routes/camera.py
+++ backend/app/routes/camera.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, UploadFile, HTTPException , APIRouter
 from datetime import datetime, timezone
 from app.schemas.frame_meta import FrameMeta
 
+frame_store: dict[int, FrameMeta] = {}
+
 
 router = APIRouter()
 # Receive the frames from front end
@@ -12,10 +14,17 @@ async def receive_frames(frame_file : UploadFile,frame_id : int):
     if frame_file.content_type not in ("image/jpeg", "image/png"): # checks if file type is images
         raise HTTPException(status_code = 415, detail = "Jpeg and PNG only file type supported") # if not gives error code
     imgbytes = await frame_file.read() # waits to read the image 
-    return{ # for now returns some info about the image
-        "frame_id": frame_id,
-        "content_type": frame_file.content_type,
-        "num_bytes" : len(imgbytes),
-        "timestamp" : datetime.now(timezone.utc)
-    }
+    meta_data = FrameMeta(
+    frame_id=frame_id,
+    content_type=frame_file.content_type,
+    num_bytes=len(imgbytes),
+    timestamp=datetime.now(timezone.utc)
+)
+    frame_store[frame_id] = meta_data
+    return meta_data
 
+@router.get("/frames/{frame_id}", response_model = FrameMeta)
+def search_frames(frame_id : int):
+    if frame_id not in frame_store:
+        raise HTTPException(status_code = 404, detail = "Frame Not Found")
+    return frame_store[frame_id]
